152b09be0a13df7a9b417c14e7e0e216a02163b5
Lera <may_be_lera@inbox.ru>
2025-05-21 00:17:23 +0300

    ...

--- js/script.js
+++ js/script.js
@@ -14,3 +14,35 @@ function burgerInit(e){
         document.body.classList.remove('body--opened-menu')
     }
 }
+
+
+
+const slides = document.querySelector('.gallery__slides');
+const slide = document.querySelectorAll('.gallery__slide');
+const prevBtn = document.querySelector('.gallery__arrows-prev');
+const nextBtn = document.querySelector('.gallery__arrows-next');
+
+let currentIndex = 0;
+const totalSlides = slide.length;
+
+function updateSliderPosition() {
+  slides.style.transform = `translateX(-${currentIndex * 105}%)`;
+}
+
+nextBtn.addEventListener('click', () => {
+  if (currentIndex < totalSlides - 1) {
+    currentIndex++;
+  } else {
+    currentIndex = 0; // Возврат к первому слайду
+  }
+  updateSliderPosition();
+});
+
+prevBtn.addEventListener('click', () => {
+  if (currentIndex > 0) {
+    currentIndex--;
+  } else {
+    currentIndex = totalSlides - 1; // Переход на последний слайд
+  }
+  updateSliderPosition();
+});
